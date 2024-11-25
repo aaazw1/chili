f21960f08337ee7fc1f5ce335cfb2e4cb91e1c32
aaazw1 <raphael.pape0@gmail.com>
2024-11-25 20:25:02 +0000

    ufufuyfyf

--- machin.py
+++ machin.py
@@ -1,7 +1,43 @@
-import PyPDF2
-with open("nomina-ppt.pdf", "rb") as pdf_file:
-    read_pdf = PyPDF2.PdfFileReader(pdf_file)
-    number_of_pages = read_pdf.getNumPages()
-    page = read_pdf.pages[0]
-    page_content = page.extractText()
-print(page_content)
+from regex import *
+from PyPDF2 import *
+name1=[]
+textT=""
+text=""
+reader = PdfReader("nomina-ppt.pdf")
+nbpage=4
+for nbpage in range(len(reader.pages)):
+#for nbpage in range(1):
+    page = reader.pages[nbpage]
+    text=page.extract_text()
+    textT=textT+page.extract_text()
+#print(text)
+    words_pattern = '[a-zA-ZÁíéúñóá]+'
+    output = findall(words_pattern, text)
+    name1=name1+output
+print("1")
+K = "K"
+while(K in name1):
+    name1.remove(K)
+print("2")
+#while(RUN in name1):
+    #name1.remove(RUN)
+for loop in range(10):
+    name1.remove(name1[0])
+for loop in range(10):
+    name1.remove(name1[-1])
+print(name1)
+RUN='RUN'
+nameF=[]
+loop=0
+merge=''
+for loop in range(len(name1)):
+    cond=1
+    if name1[loop]==RUN:
+        cond=0
+    else:
+        merge=merge+' '+name1[loop]
+    if cond==0:
+        nameF.append(merge)
+        merge=""
+        
+print(nameF)
